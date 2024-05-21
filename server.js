6aaeb13331decd874188752b5d85683df46d7d5d
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-21 15:20:31 +0200

    feat: utilizzo variabili locali nel file env

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
+
 const http = require("http");
 
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
+const word = process.env.WORD || "Hello World";
 
 http
   .createServer((req, res) => {
     res.writeHead(200, {
       "Content-Type": "text/html",
     });
-    res.end("<h1>Il mio server</h1>");
+    res.end(`<h1>${word}</h1>`);
   })
   .listen(port, host, () => {
     console.log(`Server avviato su http://${host}:${port}`);
