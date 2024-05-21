94ec8053b0f1ec83e533eaff27a9eef98dd85222
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-21 15:37:03 +0200

    feat: bonus

---
+++ function.js
@@ -0,0 +1,22 @@
+// function.js
+const quotes = [
+  "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+  "The way to get started is to quit talking and begin doing. - Walt Disney",
+  "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+  "If life were predictable it would cease to be life, and be without flavor. - Eleanor Roosevelt",
+  "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
+  "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. - James Cameron",
+  "Life is what happens when you're busy making other plans. - John Lennon",
+  "Spread love everywhere you go. Let no one ever come to you without leaving happier. - Mother Teresa",
+  "When you reach the end of your rope, tie a knot in it and hang on. - Franklin D. Roosevelt",
+  "Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead",
+];
+
+function randomWord() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+module.exports = {
+  randomWord,
+};
--- server.js
+++ server.js
@@ -2,16 +2,20 @@ require("dotenv").config();
 
 const http = require("http");
 
+const { randomWord } = require("./function");
+
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
 const word = process.env.WORD || "Hello World";
 
 http
   .createServer((req, res) => {
+    const random = randomWord();
+
     res.writeHead(200, {
       "Content-Type": "text/html",
     });
-    res.end(`<h1>${word}</h1>`);
+    res.end(`<h1>${word}</h1> <h1>${random}</h1>`);
   })
   .listen(port, host, () => {
     console.log(`Server avviato su http://${host}:${port}`);
