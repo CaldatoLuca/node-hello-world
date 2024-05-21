5a881bfca8d169d16d24db5be2c9cd39a723ee1d
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-21 15:04:05 +0200

    feat: creazione server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("ciaoooo");
+const http = require("http");
+
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>Il mio server</h1>");
+  })
+  .listen(port, host, () => {
+    console.log(`Server avviato su http://${host}:${port}`);
+  });
